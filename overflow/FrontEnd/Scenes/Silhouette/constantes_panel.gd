7dc03a08371e39d35737d6e1f6e9b0ca8c31782e
SullyvanStudio <sullyvan.studio@gmail.com>
2026-08-19 08:31:41 +0200

    Add files via upload

--- overflow/FrontEnd/Scenes/Silhouette/constantes_panel.gd
+++ overflow/FrontEnd/Scenes/Silhouette/constantes_panel.gd
@@ -7,20 +7,20 @@ extends Control
 var constantes_etiquette_array: Array = []
 var patient_constantes : PatientConstantes:
 	set(value):
-		if value!= patient_constantes:
+		if value != patient_constantes:
 			patient_constantes = value
 			constante_changed()
 
 func constante_changed() -> void:
-	for constante in PathologieLoader.constantes:
+	var constantes_triees := PathologieLoader.constantes.duplicate()
+	constantes_triees.sort_custom(func(a, b): return a.ordre_affichage < b.ordre_affichage)
+	for constante in constantes_triees:
 		var valeur = patient_constantes.get_valeur(constante)
-		var palier = patient_constantes.get_palier(constante)
-		var _palier_nom = palier.nom if palier else "hors paliers définis"
 		creer_constante(constante, valeur)
 
 func creer_constante(constante, valeur) -> void:
 	var constante_node = constantes_etiquette_scene.instantiate()
-	var string : String = "%.1f %s" % [valeur, constante.unite]
-	constante_node.label.text = string
+	var format_string : String = "%%.%df %%s" % constante.decimales
+	constante_node.label.text = format_string % [valeur, constante.unite]
 	constante_node.texture.texture = constante.texture
 	grid_container.add_child(constante_node)

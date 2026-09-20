65db0e6c016bf326a571e924c566a528aa130bdd
SullyvanStudio <sullyvan.studio@gmail.com>
2026-09-20 14:31:02 +0200

    Delete overflow/FrontEnd directory

--- overflow/FrontEnd/Components/factory.gd
+++
@@ -1,8 +0,0 @@
-class_name Factory extends Object
-
-static func create_text(text_id : StaticConst.TEXT) -> Control:
-	var scene_path : String = StaticConst.TEXT_SCENES[text_id]
-	var scene : PackedScene = load(scene_path)
-	var instance : Control = scene.instantiate()
-	return instance
-	
